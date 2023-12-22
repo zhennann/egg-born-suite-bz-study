c7c63adf73b3d32998ad407437ab0d7cf7008005
zhennann <zhen.nann@icloud.com>
2023-12-22 16:57:41 +0800

    feat: video

--- modules/study-admin/backend/src/bean/version.manager/init/init1.js
+++ modules/study-admin/backend/src/bean/version.manager/init/init1.js
@@ -1,6 +1,17 @@
+const moduleInfo = module.info;
+const __atomClassVideo = {
+  module: moduleInfo.relativeName,
+  atomClassName: 'video',
+};
 module.exports = class VersionInit {
   async run(options) {
-    // add role rights
+    // rights
+    await this._rights();
+    // categories
+    await this._categories();
+  }
+
+  async _rights() {
     const roleRights = [
       { roleName: 'authenticated', action: 'create' },
       { roleName: 'authenticated', action: 'read', scopeNames: 0 },
@@ -15,5 +26,29 @@ module.exports = class VersionInit {
       { roleName: 'system', action: 'clone', scopeNames: 'authenticated' },
     ];
     await this.ctx.bean.role.addRoleRightBatch({ atomClassName: 'video', roleRights });
+  }
+
+  async _categories() {
+    const categories = [
+      { categoryName: '产品开发', categoryIdParent: 0, categorySorting: 1 },
+      { categoryName: '产品设计', categoryIdParent: '产品开发', categorySorting: 1 },
+      { categoryName: '前端开发', categoryIdParent: '产品开发', categorySorting: 2 },
+      { categoryName: '后端开发', categoryIdParent: '产品开发', categorySorting: 3 },
+      { categoryName: '销售技巧', categoryIdParent: 0, categorySorting: 2 },
+      { categoryName: '职场提升', categoryIdParent: 0, categorySorting: 3 },
+    ];
+    const categoryIds = {};
+    for (const item of categories) {
+      // add
+      const categoryId = await this.ctx.bean.category.add({
+        atomClass: __atomClassVideo,
+        data: {
+          categoryName: item.categoryName,
+          categorySorting: item.categorySorting,
+          categoryIdParent: item.categoryIdParent ? categoryIds[item.categoryIdParent] : 0,
+        },
+      });
+      categoryIds[item.categoryName] = categoryId;
+    }
   }
 };
