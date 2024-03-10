8598297502fdf5cbf67c3f3b08b08845c861baf9
zhennann <zhen.nann@icloud.com>
2024-03-10 10:02:10 +0800

    refactor: db & model

--- modules/study-admin/src/bean/version.manager/update/update1.ts
+++ modules/study-admin/src/bean/version.manager/update/update1.ts
@@ -3,18 +3,10 @@ import { BeanBase } from '@cabloy/core';
 export class VersionUpdate extends BeanBase {
   async run(_options) {
     // create table: studyVideo
-    const sql = `
-      CREATE TABLE studyVideo (
-        id int(11) NOT NULL AUTO_INCREMENT,
-        createdAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        updatedAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        deleted int(11) DEFAULT '0',
-        iid int(11) DEFAULT '0',
-        atomId int(11) DEFAULT '0',
-        description varchar(255) DEFAULT NULL,
-        PRIMARY KEY (id)
-      )
-    `;
-    await this.ctx.model.query(sql);
+    await this.bean.model.createTable('studyVideo', function (table) {
+      table.basicFields();
+      table.atomId();
+      table.description();
+    });
   }
 }
