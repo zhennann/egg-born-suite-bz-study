2c14153efa50e3b862cf0ede40900548c265dc24
zhennann <zhen.nann@icloud.com>
2024-03-10 17:00:16 +0800

    feat: entity

--- modules/study-admin/src/model/video.ts
+++ modules/study-admin/src/model/video.ts
@@ -1,4 +1,6 @@
-import { BeanModelBase, Model } from '@cabloy/core';
+import { Model } from '@cabloy/core';
+import { BeanModelBase } from 'cabloy-module-api-a-database';
+import { EntityVideo } from '../entity/video.js';
 
 @Model({
   table: 'studyVideo',
@@ -6,4 +8,4 @@ import { BeanModelBase, Model } from '@cabloy/core';
     disableDeleted: false,
   },
 })
-export class ModelVideo extends BeanModelBase {}
+export class ModelVideo extends BeanModelBase<EntityVideo> {}
