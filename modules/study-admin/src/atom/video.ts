3f5132de1985f7cc0c7e15df8ba3838a881550e7
zhennann <zhen.nann@icloud.com>
2024-03-12 21:56:41 +0800

    feat: atom class

--- modules/study-admin/src/atom/video.ts
+++ modules/study-admin/src/atom/video.ts
@@ -1,9 +1,9 @@
-import { __ThisModule__ } from '../resource/this.js';
+import { ScopeModule } from '../resource/this.js';
 import { Atom } from '@cabloy/core';
 import { BeanAtomBase } from 'cabloy-module-api-a-base';
 
 @Atom()
-export class AtomVideo extends BeanAtomBase {
+export class AtomVideo extends BeanAtomBase<ScopeModule> {
   get model() {
     return this.scope.model.video;
   }
